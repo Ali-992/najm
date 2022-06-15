6c234f6bd28642145e6c412cf155ae576c3bdc94
Borio991 <70284513+Borio991@users.noreply.github.com>
2022-06-15 13:37:36 +0200

    add simple animations

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 export default function Title({ children }) {
   return (
-    <h2 className="mb-8 text-2xl font-bold uppercase tracking-widest text-cta md:text-3xl">
+    <motion.h2
+      className="mb-8 text-2xl font-bold uppercase tracking-widest text-cta md:text-3xl"
+      initial={{ x: -100, opacity: 0 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      transition={{ duration: 1, ease: 'easeInOut' }}
+    >
       {children}
-    </h2>
+    </motion.h2>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Page from '../components/layout/Page'
 import Hero from '../components/HomePage/Hero'
 import AboutUs from '../components/HomePage/AboutUs'
-import Partners from '../components/HomePage/Partners'
 import Services from '../components/HomePage/Services'
 import TrustedBy from '../components/HomePage/TrustedBy'
 import Contact from '../components/HomePage/Contact'
@@ -12,7 +11,6 @@ export default function HomePage() {
     <Page>
       <Hero />
       <AboutUs />
-      {/* <Partners /> */}
       <Services />
       <TrustedBy />
       <Contact />
